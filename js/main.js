c21482967f9ffddb3534a6c29c4669b636a21444
ApcarJo <hansokaz@gmail.com>
2021-05-06 15:05:02 +0200

    Arreglado la iteración para que ponga el número correcto de rombos

--- js/main.js
+++ js/main.js
@@ -2,27 +2,8 @@
 let vacio="";
 let aster="";
 let numero= parseInt(prompt("Escribe el número que será el ancho del rombo"));
-let espacios=numero;
+let espacios;
 
-
-// for (let i=0; i<5; i++){
-//     vacio+="1";
-// }
-// console.log(vacio, aster);
-
-// for (let j=0; j<5; j++){
-//     vacio-="1";
-//     console.log(vacio, aster);
-// }
-
-// for (let i=0; i<5; i++){
-
-//     for (j=0; j<5; j++){
-//         vacio-=" ";
-//     }
-//     aster+="*";
-//     console.log(vacio, aster);
-// }
 
 do {
     vacio="";
@@ -36,4 +17,4 @@ do {
     aster+="*";
     // console.log("numero es", numero);
     console.log(vacio, aster);
-}while (numero>1); 
+}while (numero>=1); 
