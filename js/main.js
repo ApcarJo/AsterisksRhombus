19eacf0e95f0d494e0f0f9dea512c647e15dd49c
ApcarJo <hansokaz@gmail.com>
2021-05-06 16:11:16 +0200

    Relleno de los espacios

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ let espacios;
 
 // Pirámide izquierda
 
-do {
-    vacio="";
-    espacios=numero-1;
-    while (espacios>=1){
-        vacio+=" ";
-        espacios--;
-        // console.log("espacios es", espacios);
-    }
-    numero--;
-    aster+="*"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
-    // console.log("numero es", numero);
-    console.log(vacio + aster);
-}while (numero>=1); 
+// do {
+//     vacio="";
+//     espacios=numero-1;
+//     while (espacios>=1){
+//         vacio+=" ";
+//         espacios--;
+//         // console.log("espacios es", espacios);
+//     }
+//     numero--;
+//     aster+="*"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
+//     // console.log("numero es", numero);
+//     console.log(vacio + aster);
+// }while (numero>=1); 
 
 // Pirámide arriba
 
@@ -36,37 +36,37 @@ do {
 //     numero--;
 //     aster+="**"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
 //     // console.log("numero es", numero);
-//     console.log(vacio + aster);
+//     console.log(vacio + aster + vacio);
 // }while (numero>=1); 
 
 // Pirámide completa
 
-// do {
-//     vacio="";
-//     espacios=numero-1;
-//     while (espacios>=1){
-//         vacio+=" ";
-//         espacios--;
-//         // console.log("espacios es", espacios);
-//     }
-//     numero--;
-//     aster+="**"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
-//     // console.log("numero es", numero);
-//     console.log(vacio + aster);
-// }while (numero>=1); 
+do {
+    vacio="";
+    espacios=numero-1;
+    while (espacios>=1){
+        vacio+="@";
+        espacios--;
+        // console.log("espacios es", espacios);
+    }
+    numero--;
+    aster+="**"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
+    // console.log("numero es", numero);
+    console.log(vacio + aster + vacio);
+}while (numero>=1); 
 
 
-//        vacio="";
-// do {
-//     aster="";
-//     espacios=numero2-1;
-//     while (espacios>=1){
-//         aster+="**";
-//         espacios--;
-//         // console.log("espacios es", espacios);
-//     }
-//     numero2--;
-//     vacio+=" ";
-//     // console.log("numero es", numero);
-//     console.log(vacio + aster);
-// }while (numero2>=1); 
+       vacio="";
+do {
+    aster="";
+    espacios=numero2-1;
+    while (espacios>=1){
+        aster+="**";
+        espacios--;
+        // console.log("espacios es", espacios);
+    }
+    numero2--;
+    vacio+="@";
+    // console.log("numero es", numero);
+    console.log(vacio + aster + vacio);
+}while (numero2>=1); 
