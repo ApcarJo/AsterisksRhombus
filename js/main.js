41db75588789ff28b2fce64ef0c95f07f7e56b7c
ApcarJo <hansokaz@gmail.com>
2021-05-06 15:49:05 +0200

    Pirámide invertida conseguida con un loop do while con las condiciones invertidas

--- js/main.js
+++ js/main.js
@@ -3,19 +3,39 @@ console.clear();
 let vacio="";
 let aster="";
 let numero= parseInt(prompt("Escribe el número que será el ancho del rombo"));
+let numero2=numero;
 let espacios;
+let acumulador1;
+
 
 
 do {
     vacio="";
     espacios=numero;
-    while (espacios>1){
+    while (espacios>=1){
         vacio+=" ";
         espacios--;
         // console.log("espacios es", espacios);
     }
     numero--;
-    aster+="*";
+    aster+="**"; // con un solo * se hace la parte izquierda del rombo, con 2 * se hace la pirámide
+    // acumulador=vacio;
     // console.log("numero es", numero);
     console.log(vacio, aster);
 }while (numero>=1); 
+
+    vacio="";
+do {
+    aster="";
+    espacios=numero2;
+    while (espacios>=1){
+        aster+="**";
+        espacios--;
+        // console.log("espacios es", espacios);
+    }
+    numero2--;
+    vacio+=" ";
+    // acumulador=vacio;
+    // console.log("numero es", numero);
+    console.log(vacio, aster);
+}while (numero2>=1); 
